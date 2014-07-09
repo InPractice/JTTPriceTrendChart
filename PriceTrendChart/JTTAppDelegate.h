7f4575abd631abe9bb635939d30cf54538e37aba
julius <w8858602>
2014-07-09 14:39:12 +0800

    Initial Commit

---
+++ PriceTrendChart/JTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTTAppDelegate.h
+//  PriceTrendChart
+//
+//  Created by julius on 14-7-9.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
